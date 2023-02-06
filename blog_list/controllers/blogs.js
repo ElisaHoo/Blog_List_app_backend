ea1a41a803818675ade720afe6691a6d81542ac1
Elisa <elisa.harjunpaa@student.hamk.fi>
2023-02-06 12:51:13 +0200

    Backend tests with SuperTest added

--- blog_list/controllers/blogs.js
+++ blog_list/controllers/blogs.js
@@ -10,13 +10,26 @@ blogsRouter.get('/', (request, response) => {
   })
   
   blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
+    const body = request.body
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes || 0
+    })
+    if (blog.title !== undefined || blog.title !== "" || blog.url !== undefined || blog.url !== "" ) {
+      blog
+        .save()
+        .then(result => {
+          response.status(201).json(result)
+        })
+    } else {
+      blog
+        .then(() => {
+          response.status(400).end()
+        })
+    }
   })
 
   module.exports = blogsRouter
---
+++ blog_list/tests/blog_api.test.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+const api = supertest(app)
+
+// Initializing test database
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(helper.initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(helper.initialBlogs[1])
+    await blogObject.save()
+  })
+
+test('There are two blogs in the database', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test('Document\'s identificator is "id"', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined()
+})
+
+test('Adding a blog is working', async () => {
+    const response = await api.post('/api/blogs').send(helper.newBlog)
+    expect(response.statusCode).toBe(201)
+    expect(response.body.title).toBe(helper.newBlog['title'])
+})
+
+test('If no value is given to likes, value will be 0', async () => {
+    const response = await api.post('/api/blogs').send(helper.newBlogWithoutLikes)
+    expect(response.body.likes).toBe(0)
+})
+
+test('If no value is given to title or url, response statuscode is 400', async () => {
+    const response = await api.post('/api/blogs').send(helper.newBlogWithoutTitle).send(helper.newBlogWithoutUrl)
+    expect(response.body.title).toBe("React patterns")
+    expect(response.body.url).toBe("https://reactpatterns.com/")
+})
+
+afterAll(async () => {
+    await mongoose.connection.close()
+})
